4498d9cfdd42c008caf114c4d3d3fc099aacb5f3
Danilo <danilo.dante01@hotmail.com>
2025-05-08 20:40:31 -0300

    feat:check-data-hook

---
+++ src/core/hook/useCheckData.tsx
@@ -0,0 +1,36 @@
+import { api } from "@/lib/axios";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { UserType } from "../types/user-type";
+
+
+
+export function useCheckData(email?: string) {
+    const router = useRouter();
+  
+    useEffect(() => {
+      if (!email) return;
+  
+      async function getData() {
+        try {
+          const response = await api.get<UserType>("/sign-in/check-data", {
+            params: { email },
+          });
+          
+          const user = response.data;
+          console.log(user);
+          
+          if (!user.cpf || !user.gender || !user.phone) {
+            router.push('/check-data')            
+          } else {
+            router.push('/home')            
+          }
+        } catch (err) {
+          console.error("Erro ao buscar usuário:", err);
+       
+        }
+      }
+  
+      getData();
+    }, [email, router]);
+  }
