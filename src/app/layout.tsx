49ea12a149299f9f35b0bbdb359c51c7971b91d5
Danilo <danilo.dante01@hotmail.com>
2025-05-08 15:45:25 -0300

    feat:query-client-config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,34 @@
-import type { Metadata } from "next";
+'use client'
+
+import AuthProvider from "@/core/providers/auth-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../core/styles/globals.css";
 import "../core/styles/tailwind-config.css";
 
-export const metadata: Metadata = {
-  title: "LAV",
-  description: "Lav.",
-};
+
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const queryClient = new QueryClient();
+
   return (
     <html lang="pt-BR">
       <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" />
-<link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
       </head>
-      <body className={`font-baseText antialiased`}>{children}</body>
+      <body className={`font-baseText antialiased`}>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>{children}</AuthProvider>
+        </QueryClientProvider>
+      </body>
     </html>
   );
 }
