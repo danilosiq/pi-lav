6fe4cc61cd8616ac6ff652e6bf0553694d2e4083
Danilo <danilo.dante01@hotmail.com>
2025-05-08 20:42:40 -0300

    chore:adding-cpf-on-userType

--- src/core/types/user-type.ts
+++ src/core/types/user-type.ts
@@ -3,6 +3,7 @@ import { BaseType } from "./base-type";
 export type UserType = BaseType & {
   name: string;
   email: string;
+  cpf:string;
   phone: string;
   gender: "masculino" | "feminino" | "outro";
 };
