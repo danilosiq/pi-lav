1f46ebc8ab7088fd2242076c898b710b0f3ff52f
Danilo <danilo.dante01@hotmail.com>
2025-05-13 21:40:17 -0300

    adding-auth-feature

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,13 @@
 "use client"
 
+import { useGetUser } from "@/core/hook/useGetUser";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Profile() {
   const router = useRouter();
+  const { isLoading } = useGetUser("GetAuth");
+
 
   useEffect(() => {
     router.push("/profile/profile-data");
